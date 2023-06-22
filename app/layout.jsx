813454985a7ab7a7965c054fd7e8aa0ce73098ed
Adhithya Srivatsan <addytrunks@gmail.com>
2023-06-22 14:05:19 +0530

    Page Setup

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: 'Promptopia',
+    description:"Discover and share AI prompts"
+}
+
+const Layout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+
+            <main className='app'>
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default Layout;
---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='w-full flex-col flex-center'>
+        <h1 className='head_text text-center'>
+            Discover and Share
+            <br className='max-md:hidden' />
+            <span className='orange_gradient text-center'>AI-Powered Prompts</span>
+        </h1>
+        <p className='desc text-center'>
+            Promptopia is an open source AI prompting tool for modern world to dicsover, create and share creative prompts
+        </p>
+    </section>
+  )
+}
+
+export default Home
